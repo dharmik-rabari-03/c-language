eb4691b0d9de6a2b16b509c45c1599a130bbe058
Dharmik rabari <dharmik161616@gmail.com>
2025-10-17 16:56:27 +0530

    Add files via upload

---
+++ loops/while.c
@@ -0,0 +1,49 @@
+
+
+    // example-1
+
+#include <stdio.h>
+
+
+int main(){
+
+    int num=1,sum=0;
+
+
+    while(num<=100){
+       
+        sum+=num;
+        num++;
+    
+    }
+
+ printf("%d\n",sum);
+    return 0;
+}
+
+
+
+
+// example-2
+
+
+
+
+// #include <stdio.h>
+
+
+// int main(){
+
+//     int num=1;
+
+
+//     while(num<=10){
+//         printf("%d\n",num);
+//         num++;
+    
+//     }
+
+
+//     return 0;
+// }
+
