0ebcba851bac817026d5b145e8d86f01de2dec8f
Dharmik rabari <dharmik161616@gmail.com>
2025-11-26 16:41:00 +0530

    Add files via upload

---
+++ CH10-ARRAY/static.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+    int number[5]={};
+
+    number[0]=5;
+
+    number[1]=9;
+
+    number[2]=7;
+
+    number[3]=8;
+
+    number[4]=6;
+
+
+    printf("%d\n",number[1]);
+    printf("%d\n",number[0]);
+    printf("%d\n",number[3]);
+    printf("%d\n",number[2]);
+
+
+    return 0;
+}
