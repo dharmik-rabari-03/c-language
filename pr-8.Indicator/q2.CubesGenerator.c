6f7623e9329719c8cc58895902de4023b17a1bcd
Dharmik rabari <dharmik161616@gmail.com>
2025-12-17 10:59:02 +0530

    Add files via upload

---
+++ pr-8.Indicator/q2.CubesGenerator.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void cube(int *arr, int size)
+{
+    int i, j;
+
+    printf("Cubes of all elements:\n");
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            int value = *(arr + i * size + j);
+            printf("%d ", value * value * value);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int a[10][10];
+    int size, i, j;
+
+    printf("Enter array's size: ");
+    scanf("%d", &size);
+
+    printf("Enter array elements:\n");
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            printf("a[%d][%d] = ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    cube(&a[0][0], size);
+
+    return 0;
+}
